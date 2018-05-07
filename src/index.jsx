80b4e306022bc0e7eddf6d04029ee4a61446c8cc
quelledanielle <quelledanielle@users.noreply.github.com>
2018-05-07 11:52:45 -0400

    Remove comments, log

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import AppContainer from './AppContainer';
-// import App from './App.jsx';
-
-console.log(store)
 
 ReactDOM.render(
   <Provider store={store}>
