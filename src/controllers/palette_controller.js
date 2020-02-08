ef0ab48d89ec39e49f03cb0cd661b75cbca7df60
D Pham <1707217+dphm@users.noreply.github.com>
2020-02-08 16:23:39 -0500

    Do a color-changey thing

--- src/controllers/palette_controller.js
+++ src/controllers/palette_controller.js
@@ -1,16 +1,53 @@
 import { Controller } from "stimulus"
+import PaletteData from "../palette-data"
 
 export default class extends Controller {
     static targets = [
+        "screen",
         "canvas",
         "selector"
     ]
 
-    connect() {
-        const canvas = this.canvasTarget;
-        console.log(canvas);
+    get colors() {
+        return this.paletteData.colors(this.selectorTarget.value)
+    }
 
-        const selector = this.selectorTarget;
-        console.log(selector);
+    get rgbColors() {
+        return this.colors.map((color) => color.rgbString)
+    }
+
+    connect() {
+        this.loadData()
+    }
+
+    loadData() {
+        fetch(this.data.get("url"))
+            .then((response) => response.text())
+            .then((palettesJson) => {
+                const palettes = JSON.parse(palettesJson).palettes
+                this.paletteData = new PaletteData(palettes)
+                this.initializeOptions()
+                this.update()
+            })
+    }
+
+    update() {
+        this.screenTarget.style = `background-color: ${this.rgbColors[3]};`
+        this.canvasTarget.style = `background-color: ${this.rgbColors[2]};`
+        this.selectorTarget.style = `color: ${this.rgbColors[0]};`
+        this.selectorTarget.parentElement.style = `border-color: ${this.rgbColors[1]}; box-shadow: inset 0 2px ${this.rgbColors[2]}, 0 2px ${this.rgbColors[2]};`
+    }
+
+    initializeOptions() {
+        const options = this.paletteData.names.map((name) => {
+            var option = document.createElement("option")
+            option.className = "palette-name"
+            option.value = name
+            option.innerText = name
+            return option
+        })
+
+        const selector = this.selectorTarget
+        options.forEach((option) => selector.appendChild(option))
     }
 }
